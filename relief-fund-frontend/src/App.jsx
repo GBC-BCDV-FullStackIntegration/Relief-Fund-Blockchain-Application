687a9059013dc718b8053b2e0a4c759889a889ee
NithinNitz12 <nithinraj10@hotmail.com>
2024-04-17 02:20:45 -0400

    💄 UI improved

--- relief-fund-frontend/src/App.jsx
+++ relief-fund-frontend/src/App.jsx
@@ -4,6 +4,16 @@ import axios from "axios";
 import { ethers } from "ethers";
 import ReliefFundABI from "../artifacts/contracts/ReliefFund.sol/ReliefFund.json";
 
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
 function App() {
   const [account, setAccount] = useState("");
   const [amount, setAmount] = useState("");
@@ -54,6 +64,8 @@ function App() {
         const signer = provider.getSigner();
         const address = await signer.getAddress();
         setAccount(address);
+        fetchTotalDonations(); // Fetch total donations
+        fetchDonations(); // Fetch all donations details
       } catch (error) {
         console.error("Error connecting to MetaMask:", error);
       }
@@ -118,24 +130,65 @@ function App() {
     <div className="App">
       <h1>Relief Fund</h1>
       <button onClick={connectMetaMask}>Connect MetaMask</button>
-      <p>Connected Account: {account}</p>
-      <p>Total Donations: {totalDonations} ETH</p>
-      <input
-        type="text"
-        placeholder="Amount (ETH)"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <button onClick={donate}>Donate</button>
+      <h2>Connected Account: {account}</h2>
+      <h3>Total Donations: {totalDonations} ETH</h3>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          type="text"
+          label="Amount (ETH)"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          type="text"
+          label="Message"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <br />
+        <button onClick={donate}>Donate</button>
+      </Box>
       <button onClick={withdraw}>Withdraw</button>
       <h2>Donations List</h2>
-      <ul>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 1000 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Donor</TableCell>
+              <TableCell>Amount</TableCell>
+              <TableCell>Message</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {donations &&
+              donations.map((donation, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell>{donation.donor}</TableCell>
+                  <TableCell> {donation.amount} ETH</TableCell>
+                  <TableCell> {donation.description}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {/* <ul>
         {donations &&
           donations.map((donation, index) => (
             <li key={index}>
@@ -144,7 +197,7 @@ function App() {
               <p>Description: {donation.description}</p>
             </li>
           ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
